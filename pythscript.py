f22913edbadd5c439d8f65de32aca2f5552f5200
Jakester2000 <mrjrbrown13@gmail.com>
2019-11-01 09:32:13 +0000

    uncomment matching line

--- pythscript.py
+++ pythscript.py
@@ -1,25 +1,32 @@
 #set this to the path where the file with the server information is kept
 FilePath = input("whats the file called? >>>  ")
 UserInput = input("What line you want \"#\" to be removed? >>>  ")
-def write(file, to_write):
-	with open(file, 'a+') as f:
-		f.write(f'{to_write}\n')
 
-
-def delete(file, to_delete):
+def checkTitle(file, to_check):
+	found = False
 	with open(file, 'r') as f:
 		lines = f.readlines()
+			
 
 	with open(file, 'w') as f:
-		for line in lines:
-			if line.strip('\n') != to_delete:
-				f.write(line)
-			else:
-				f.write(line.strip('#'))
+	
+		while not found:
+			for line in lines:
+				if line.strip('\n') == str("#" + to_check):
+					print("Found")
+					found = True
+					f.write(line.strip("#"))
+
+				else:
+
+					f.write(line)
+			
+
+
+checkTitle(FilePath, UserInput)
+
+	
 
 
 
-for i in range(0, 50):
-	write(FilePath, "#line number" + str(i))
-
-delete(FilePath, UserInput)
+# delete(FilePath, UserInput)
